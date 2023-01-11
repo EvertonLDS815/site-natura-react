d5ae2b66abc7a99c0545d7bd86ef58094d682acb
EvertonLDS815 <user.name>
2023-01-11 10:44:45 -0300

    alteração 2

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -4,7 +4,7 @@ function Copyright() {
 
     return (
         <Copyrightpage className="copyright">
-            <p>&#169; 2022 - <a href="https://everton-l-portifolio.netlify.app">EL Tutoriais</a> All Right Reserved</p>
+            <p>&#169; 2023 - <a href="https://everton-l-portifolio.netlify.app">EL Tutoriais</a> All Right Reserved</p>
         </Copyrightpage>
     )
 }
